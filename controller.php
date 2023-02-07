026fc6e89db9dd28b576c571bb9279987e907ef7
Yancap <yangabriel3001f@gmail.com>
2023-02-07 10:31:13 -0300

    "iniciando o controller"

--- controller.php
+++ controller.php
@@ -1,5 +1,21 @@
 <?php 
+    include("dal.php");
+    $Dal = new Dal();
+    class Controller{
+        function __construct(){
+            
+        }
+        
+        public function validation($data,$checkOf){
+            if ($checkOf === "user") {
+                
+            } elseif ($checkOf === "email"){
 
+            } else{
 
+            }
+        }
+    }
+    
 
 ?>
